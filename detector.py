9ec390f95fc86d5f24c24815bd0563a4d1bdd9d6
PuneethKal <puneeth.kallem@gmail.com>
2024-07-28 00:47:51 -0400

    Initial Files

---
+++ detector.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+
+import face_recognition
+import pickle
+
+DEFAULT_ENCODINGS_PATH = Path("output/encodings.pkl")
+
+Path("training").mkdir(exist_ok=True)
+Path("validation").mkdir(exist_ok=True)
+Path("output").mkdir(exist_ok=True)
+
+def encode_known_faces(
+        model: str = "hog", encodings_location: Path = DEFAULT_ENCODINGS_PATH
+):
+
+    names = []
+    encodings = []
+    for filepath in Path("training").glob("*/*"):
+        name = filepath.parent.name
+        image = face_recognition.load_image_file(filepath)
+
+        face_locations = face_recognition.face_locations(image, model=model)
+        face_encodings = face_recognition.face_encodings(image, face_locations)
+
+        for encoding in face_encodings:
+            names.append(name)
+            encodings.append(encoding)
+        
+    name_encodings = {"names":names, "encodings": encodings}
+    with encodings_location.open(mode="wb") as f:
+        pickle.dump(name_encodings, f)
+
+# encode_known_faces()
+
+def recongize_faces(
+    image_location: str,
+    model: str="hog",
+    encodings_location: Path = DEFAULT_ENCODINGS_PATH,
+):
+
+    for bounding_box, unknown_encoding in zip(
+        input_face_locations, input_face_encodings
+    ):
+        name = _recognize_face(unknown_encoding, loaded_encodings)
+        if not name: 
+            name = "Unknown"
+            print(name, bounding_box)
